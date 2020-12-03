8c654434958b6ccb759e95f309d1abf26ca425d7
刘东旭 <1278083770@qq.com>
2020-12-03 19:00:38 +0800

    小调整

--- 17.go
+++ 17.go
@@ -2,7 +2,7 @@
 * @Author: 刘东旭
 * @Date: 2020-11-22 12:23:14
  * @LastEditors: 刘东旭
- * @LastEditTime: 2020-11-26 15:29:26
+ * @LastEditTime: 2020-12-03 18:45:37
 * @Description:
 */
 package main
@@ -15,9 +15,11 @@ type calcType func(int, int) int
 func calc(x, y int, cFn calcType) int {
 	return cFn(x, y)
 }
+
 func add(x, y int) int {
 	return x + y
 }
+
 func main() {
 	sum := calc(10, 5, add)
 	fmt.Printf("%v", sum)
